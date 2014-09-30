902271163a293bb0684045c278d23c5c2bbeb587
Lisa Ha <lisamayha@gmail.com>
2014-09-30 14:19:56 -0700

    fixed active nav tabs

--- public/javascripts/jquery.js
+++ public/javascripts/jquery.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
 	if(location.hash === "#/") {
-		console.log('looking at resume')
 		$('#resume_tab').attr('class', 'active')
 	}
 	if(location.hash === "#/projects") {
-		console.log('looking at projects')
 		$('#projects_tab').attr('class', 'active')
 	}
 	$(document).on('click', 'a', function(){
@@ -13,11 +11,9 @@ $(document).ready(function(){
 			$('#contact_tab').attr('class', 'active')
 		}
 		if(location.hash === "#/" && $(this).parent().attr('id') != 'contact_tab') {
-			// console.log('looking at resume')
 			$('#resume_tab').attr('class', 'active')
 		}
 		if(location.hash === "#/projects" && $(this).parent().attr('id') != 'contact_tab') {
-			// console.log('looking at projects')
 			$('#projects_tab').attr('class', 'active')
 		}
 	})
